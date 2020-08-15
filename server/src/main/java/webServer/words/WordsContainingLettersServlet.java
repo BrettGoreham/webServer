60ef4c46ed8fd010a54d536374848575dd3c4385
Brett Goreham <brett.goreham@evry.com>
2020-08-15 13:27:44 +0200

    adding boxes so things are grouped by length of result. makes it easier to read

--- server/src/main/java/webServer/words/WordsContainingLettersServlet.java
+++ server/src/main/java/webServer/words/WordsContainingLettersServlet.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/words")
@@ -30,10 +31,23 @@ public class WordsContainingLettersServlet {
     public String getWords(@RequestParam String language, @RequestParam String input, @RequestParam(defaultValue = "0") int minLength, Model model) {
         List<String> words = wordsContainingLettersService.findWordsPossibleWithInputSequence(language, input.replaceAll("\\s","").toLowerCase(), minLength);
 
+        List<List<String>> groupedWords =
+            new ArrayList<>(words
+                                .stream()
+                                .collect(
+                                    Collectors.groupingBy(
+                                        String::length, HashMap::new, Collectors.toList())
+                                )
+                                .values());
+
+        // sort groups by the size want list of longer words first
+        groupedWords.sort((lista, listb) -> Integer.compare(listb.get(0).length(),lista.get(0).length()));
+
         model.addAttribute("language", LanguageCharacterSet.getLanguageCharacterSetFromLanguageCode(language).get().getFullName());
         model.addAttribute("input", input);
         model.addAttribute("minLength", minLength);
-        model.addAttribute("words", words);
+        model.addAttribute("words", groupedWords);
+
 
         return "words/wordsSearch";
     }
